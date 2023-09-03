cb6a03b8c1f83441465f0d3f61b7fd3babd2f7fa
Shashank Naik <supercellgames50@gmail.com>
2023-09-03 13:47:24 +0530

    fix.add validator to employee activity model

--- src/employeeActivity/model/employeeActivity.schema.js
+++ src/employeeActivity/model/employeeActivity.schema.js
@@ -6,11 +6,25 @@ employeeActivitySchema = new schema({
         type : mongoose.Schema.Types.ObjectId,
         ref : 'employee',
         required: true,
+        validate: {
+            validator: async function (value) {
+                const employee = await mongoose.model('employee').findById(value);
+                return employee !== null;
+            },
+            message: 'Invalid employeeId, employee does not exist.',
+        },
     },
     activityId:{
         type : mongoose.Schema.Types.ObjectId,
         ref : 'activity',
         required: true,
+        validate: {
+            validator: async function (value) {
+                const employee = await mongoose.model('activity').findById(value);
+                return employee !== null;
+            },
+            message: 'Invalid activityId, activity does not exist.',
+        },
     },
     completedAt: {
 		type: Date,

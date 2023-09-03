b56ec628644cfe24917f6bc3c2b2e3983f36d1d7
Shashank Naik <supercellgames50@gmail.com>
2023-09-03 12:46:35 +0530

    add.  crud operation activity

---
+++ src/activity/activityController.js
@@ -0,0 +1,47 @@
+const activity = require('./model/activity.schema')
+
+exports.create=(req,res)=>{
+    let newactivity = new activity({
+        name:req.body.name,
+        points:req.body.points,
+    })
+
+    newactivity.save((err,data)=>{
+        if (err) res.status(400).send(err);
+        
+        res.status(201).send(data)
+    })
+}
+
+exports.read=(req,res)=>{
+    activity.find({},{__v:0}).exec((err,data)=>{
+        if (err) res.status(400).send(err);
+
+        res.send(data)
+    })
+}
+
+exports.update=(req,res)=>{
+    activity.findByIdAndUpdate(req.params.id, req.body, (err, data) => {
+        if (err) res.status(400).send(err);
+
+        if (data === null){
+            res.status(404).send("No data found with user id "+ req.params.id)
+        }else{
+            res.send(data)
+        }
+      });
+}
+
+exports.delete=(req,res)=>{
+    activity.findByIdAndDelete(req.params.id, (err, data) => {
+        if (err) res.status(400).send(err);
+
+        if (data === null){
+            res.status(404).send("No data found with user id "+ req.params.id)
+        }else{
+            res.send(data)
+        }
+      });
+}
+
